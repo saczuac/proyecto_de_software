ab2cd88a2079e3a07c669551a0e4e0a8de6b4991
sacha <sspinelli@danaide.com.ar>
2016-09-20 15:45:33 -0300

    ejercicio 2 terminado

--- P3/ej2/productos.php
+++ P3/ej2/productos.php
@@ -21,15 +21,15 @@
       <?php
          $e = false;
          if (isset($_POST["producto"])) {
-            $pn = $_POST["producto"];
-            if ((strlen($pn) > 20) || ($pn == "")) {
+            $prod = $_POST["producto"];
+            if ((strlen($prod) > 20) || ($prod == "")) {
                $e = true;
-               echo  "<input type='text' id='productName' maxlength='20' required='true' value='$pn' class='error'/> El campo no puede estar vacío o tener menos de 20 caracteres";
+               echo  "<input type='text' name='producto' id='productName' maxlength='20' value='$prod' class='error' required/> El campo no puede estar vacío o tener menos de 20 caracteres";
             } else {
-              echo "<input type='text' id='productName' maxlength='20' required='true'/>";
+              echo "<input type='text' id='productName' name='producto' maxlength='20' required/>";
             }
           } else {
-            echo "<input type='text' id='productName' maxlength='20' required='true'/>";
+            echo "<input type='text' id='productName' name='producto' maxlength='20' required/>";
           }
       ?>
       <br><br>
@@ -38,13 +38,13 @@
          if (isset($_POST["markName"])) {
             $markName = $_POST["markName"];
             if ((strlen($markName) > 20) || ($markName == "")) {
-              $e = true;
-               echo  "<input type='text' name='markName' maxlength='20' class='error' value='$markName' required /> El campo no puede estar vacío o tener menos de 20 caracteres";
+                $e = true;
+               echo  "<input type='text' id='markName' name='markName' maxlength='20' class='error' value='$markName' required /> El campo no puede estar vacío o tener menos de 20 caracteres";
             } else {
-              echo  "<input type='text' name='markName' maxlength='20' required/>";
+              echo  "<input type='text'  id='markName' name='markName' maxlength='20' required/>";
             }
           } else {
-             echo  "<input type='text' name='markName' maxlength='20' required/>";
+             echo  "<input type='text'  id='markName' name='markName' maxlength='20' required/>";
           }
       ?>
       <br><br>
@@ -53,13 +53,13 @@
          if (isset($_POST["barcode"])) {
             $barcode = $_POST["barcode"];
             if ((strlen($barcode) > 20) || ($barcode == "")) {
-              $e = true;
-               echo "<input type='number' name='barcode' maxlength='20' class='error' value='$barcode' required/> El campo no puede estar vacío o tener menos de 20 caracteres";
+                $e = true;
+               echo "<input type='number' id='barcode' name='barcode' maxlength='20' class='error' value='$barcode' required/> El campo no puede estar vacío o tener menos de 20 caracteres";
             } else {
-              echo "<input type='number' name='barcode' maxlength='20' required/> ";
+              echo "<input type='number' id='barcode' name='barcode' maxlength='20' required/> ";
             }
           } else {
-            echo "<input type='number' name='barcode' maxlength='20' required/> ";
+            echo "<input type='number' id='barcode' name='barcode' maxlength='20' required/> ";
           }
       ?>
       <br><br>
@@ -68,15 +68,15 @@
          if (isset($_POST["category"])) {
             $category = $_POST['category'];
             if ($category == 1) {
-              $e = true;
-               echo " <select name='category' class='error'>
+               $e = true;
+               echo " <select name='category' class='error' id='select'>
                       <option value='' selected>Seleccione</option>
                       <option value='food'>Alimento</option>
                       <option value='drink'>Bebida</option>
                       <option value='library'>Librería</option>
                      </select> Debe seleccionar alguna opción";
             } else {
-              echo " <select name='category'>
+              echo " <select name='category' id='select'>
                      <option value='1' selected>Seleccione</option>
                      <option value='2'>Alimento</option>
                      <option value='3'>Bebida</option>
@@ -84,7 +84,7 @@
                     </select>";
             }
           } else {
-            echo " <select name='category'>
+            echo " <select name='category' id='select'>
                    <option value='1' selected>Seleccione</option>
                    <option value='2'>Alimento</option>
                    <option value='3'>Bebida</option>
@@ -93,8 +93,25 @@
           }
       ?>
       <br><br>
-      <input type="submit" value="Agregar"/>
+      <input type="submit" value="Agregar"/><br>
+      <a class="button" href="productos.php">Reestablecer</a>
     </form>
+    <?php
+    if ($e) {
+        echo "<script type='text/javascript'>
+        document.getElementById('markName').value = '$markName';
+        document.getElementById('barcode').value = '$barcode';
+        document.getElementById('select').value = '$category';
+        document.getElementById('productName').value = '$prod';
+        </script>";
+    } else {
+        if (isset($_POST["category"])){
+            $message = "Producto agregado con éxito!";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+        }
+    }
+
+     ?>
   </div>
     <footer>Pie de página</footer>
     </body>
